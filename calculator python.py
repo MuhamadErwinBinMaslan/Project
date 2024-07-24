2bc86b0f779bfea6cc068bfae4d0bf729599b554
MuhamadErwinBinMaslan <146042656+MuhamadErwinBinMaslan@users.noreply.github.com>
2024-07-24 15:47:14 +0800

    Add files via upload

---
+++ calculator python.py
@@ -0,0 +1,8 @@
+print("Please enter the 1st number")
+firstnumber = int(input())
+
+print('Please enter the 2nd number')
+secondnumber = int(input())
+
+a = (firstnumber * secondnumber)
+print(f"{firstnumber} * {secondnumber} = {a}")
